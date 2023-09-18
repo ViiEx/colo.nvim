b2b0b38792d2ded60823a74038ab0702f0a77865
Vasilis Christopoulos <v.christopoulos@divbyzero.gr>
2023-09-18 19:03:16 +0300

    fix: temp solution comment out the injection

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -1,8 +1,8 @@
 ; extends
 
-((function_call
-  name: (_) @_vimcmd_identifier
-  arguments: (arguments (string content: _ @query) .))
-  (#eq? @_vimcmd_identifier "vim.treesitter.query.set_query"))
+;((function_call
+;  name: (_) @_vimcmd_identifier
+;  arguments: (arguments (string content: _ @query) .))
+;  (#eq? @_vimcmd_identifier "vim.treesitter.query.set_query"))
 
-((string ("string_content") @query) (#lua-match? @query "^%s*;+%s?query"))
+;((string ("string_content") @query) (#lua-match? @query "^%s*;+%s?query"))
